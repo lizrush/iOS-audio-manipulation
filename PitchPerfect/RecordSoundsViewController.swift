e6908b5da7ad2587f1fd3d91b3240ff357a36f2b
liz rush <liz@lizmrush.com>
2017-03-06 09:55:41 -0800

    Remove empty overrides

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -17,17 +17,6 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
   @IBOutlet var recordButton: UIButton!
   @IBOutlet var stopRecordButton: UIButton!
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-
   @IBAction func recordAudio(_ sender: Any) {
     recordLabel.text = "Recording in progress..."
     stopRecordButton.isEnabled = true
